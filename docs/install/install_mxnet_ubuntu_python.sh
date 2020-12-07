0b64381c2472b8f391cf79ab6d341cd223f280bc
Ubuntu <ubuntu@ip-172-31-94-121.ec2.internal>
2020-12-07 03:52:48 +0000

    push

--- docs/install/install_mxnet_ubuntu_python.sh
+++ docs/install/install_mxnet_ubuntu_python.sh
@@ -66,7 +66,7 @@ if [ "$gpu_install" = "0" ]; then
     make_params="USE_OPENCV=1 USE_BLAS=openblas"
     echo "nvidia-smi not found. Installing in CPU-only mode with these build flags: $make_params"
 else
-    make_params="USE_OPENCV=1 USE_BLAS=openblas USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1 USE_DIST_KVSTORE=1"
+    make_params="USE_OPENCV=1 USE_BLAS=openblas USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1"
     echo "nvidia-smi found! Installing with CUDA and cuDNN support with these build flags: $make_params"
 fi
 
